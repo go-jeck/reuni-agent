4b16cbcb83fb546043bea1a1b783ccc74013e4ec
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-07-27 15:35:52 +0700

    add http method for send request

--- http.go
+++ http.go
@@ -9,10 +9,30 @@ type HttpCaller interface {
 	SendRequest() *http.Response
 }
 
+type HttpHelper struct {
+	URL           string
+	Method        string
+	Authorization string
+}
+
 func getFetchVersionURL(config *ReuniAgentConfiguration) string {
 	return fmt.Sprintf("%v/services/%v/%v/agent", config.Host, config.Service, config.Namespace)
 }
 
 func getFetchConfigurationURL(config *ReuniAgentConfiguration, version int) string {
 	return fmt.Sprintf("%v/services/%v/%v/%v/agent", config.Host, config.Service, config.Namespace, version)
+}
+
+func (h *HttpHelper) SendRequest() (*http.Response, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest(h.Method, h.URL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", h.Authorization)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
--- http_test.go
+++ http_test.go
@@ -27,3 +27,20 @@ func TestGetConfigurationUrlMethod(t *testing.T) {
 	expectedUrl := "http://127.0.0.1:8080/services/test-service/development/2/agent"
 	assert.Equal(t, expectedUrl, getFetchConfigurationURL(agentConfig, 2))
 }
+
+func TestSendRequestShouldNotReturnError(t *testing.T) {
+	helper := HttpHelper{
+		URL:    "http://example.com",
+		Method: "GET",
+	}
+	resp, err := helper.SendRequest()
+
+	assert.Equal(t, nil, err)
+	assert.NotEqual(t, nil, resp)
+}
+
+func TestSendRequestShouldReturnError(t *testing.T) {
+	helper := HttpHelper{}
+	_, err := helper.SendRequest()
+	assert.NotEqual(t, nil, err)
+}
